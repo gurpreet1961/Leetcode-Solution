ac7e56a2da4c60aaa2a5c9ee46669643b7087756
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-09-05 12:56:25 +0530

    Time: 91 ms (39.13%) | Memory: 60.8 MB (23.19%) - LeetSync

--- 1043-grid-illumination/grid-illumination.java
+++ 1043-grid-illumination/grid-illumination.java
@@ -1,85 +1,54 @@
 class Solution {
-    int[][] directions = {
-        // cur, direct horz & vertical & diagonals
-        {0, 0},
-        {1, 0}, {-1, 0}, {0, 1}, {0, -1},
-        {1, 1}, {1, -1}, {-1, -1}, {-1, 1}
-    };
+    int[][] dir = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }, { 1, 1 }, { -1, 1 }, { -1, -1 }, { 1, -1 },
+            { 0, 0 } };
 
-    Set<Integer> litLamps;
-    Map<Integer, Integer> rowLit;
-    Map<Integer, Integer> colLit;
-    Map<Integer, Integer> descendingDiagonalLit;
-    Map<Integer, Integer> ascendingDiagonalLit;
-    
-    // O(n^2) time | O(n^2) space
-    // n = board side length, worst case = all lamps lit and we iterate all lamps and add to set
-    public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
-        litLamps = new HashSet<>();
-        rowLit = new HashMap<>();
-        colLit = new HashMap<>();
-        descendingDiagonalLit = new HashMap<>();
-        ascendingDiagonalLit = new HashMap<>();
-        
+    public int[] gridIllumination(int N, int[][] lamps, int[][] queries) {
+        int[] res = new int[queries.length];
 
-        for (int[] pos : lamps) {
-            int r = pos[0], c = pos[1];
-            int posHash = r * n + c;
-            if (litLamps.contains(posHash)) continue; // manage duplicate inputs
-            litLamps.add(posHash);
-            light(pos[0], pos[1], n);
+        Map<Integer, Integer> row = new HashMap<>();
+        Map<Integer, Integer> col = new HashMap<>();
+        Map<Integer, Integer> d1 = new HashMap<>();
+        Map<Integer, Integer> d2 = new HashMap<>();
+        Map<Integer, Boolean> cellNo = new HashMap<>();
+
+        for (int i = 0; i < lamps.length; i++) {
+            int r = lamps[i][0];
+            int c = lamps[i][1];
+            if (!cellNo.containsKey(N * r + c)) {
+                row.put(r, row.getOrDefault(r, 0) + 1);
+                col.put(c, col.getOrDefault(c, 0) + 1);
+                d1.put(r - c, d1.getOrDefault(r - c, 0) + 1);
+                d2.put(r + c, d2.getOrDefault(r + c, 0) + 1);
+                cellNo.put(N * r + c, true);
+            }
         }
 
-        int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            int[] pos = queries[i];
+            int r = queries[i][0];
+            int c = queries[i][1];
 
-            if (isIlluminated(pos[0], pos[1], n)) {
-                res[i] = 1;
+            res[i] = (row.getOrDefault(r, 0) > 0 || col.getOrDefault(c, 0) > 0 || d1.getOrDefault(r - c, 0) > 0
+                    || d2.getOrDefault(r + c, 0) > 0) ? 1 : 0;
 
-                for (int[] d : directions) {
-                    int r = pos[0] + d[0], c = pos[1] + d[1];
-                    if (r < 0 || r >= n || c < 0 || c >= n) continue;
-                    int posHash = r * n + c;
-                    
-                    if (litLamps.contains(posHash)) {
-                        lightOff(r, c, n);
-                        litLamps.remove(posHash);
+            // switch off the lamps, if any
+
+            for (int[] d : dir) {
+                int r1 = r + d[0], c1 = c + d[1];
+                if (r1 >= 0 && r1 < N && c1 >= 0 && c1 < N)
+                    if (cellNo.containsKey(N * r1 + c1) && cellNo.get(N * r1 + c1)) {
+
+                        // the lamp is on, turn it off, so decrement the count of the lamps
+
+                        row.put(r1, row.getOrDefault(r1, 1) - 1);
+                        col.put(c1, col.getOrDefault(c1, 1) - 1);
+                        d1.put(r1 - c1, d1.getOrDefault(r1 - c1, 1) - 1);
+                        d2.put(r1 + c1, d2.getOrDefault(r1 + c1, 1) - 1);
+                        cellNo.put(N * r1 + c1, false);
                     }
-                }
-            } 
-            else res[i] = 0;
+            }
+
         }
         return res;
     }
 
-    private void light(int r, int c, int n) {
-        int d_d = r - c;
-        int a_d = r + c;
-
-        rowLit.put(r, rowLit.getOrDefault(r, 0) + 1);
-        colLit.put(c, colLit.getOrDefault(c, 0) + 1);
-        descendingDiagonalLit.put(a_d, descendingDiagonalLit.getOrDefault(a_d, 0) + 1);
-        ascendingDiagonalLit.put(d_d, ascendingDiagonalLit.getOrDefault(d_d, 0) + 1);
-    }
-
-    private boolean isIlluminated(int r, int c, int n) {
-        int d_d = r - c;
-        int a_d = r + c;
-        
-        return rowLit.getOrDefault(r, 0) > 0
-        || colLit.getOrDefault(c, 0) > 0 
-        || descendingDiagonalLit.getOrDefault(a_d, 0) > 0 
-        || ascendingDiagonalLit.getOrDefault(d_d, 0) > 0;
-    }
-
-    private void lightOff(int r, int c, int n) {
-        int d_d = r - c;
-        int a_d = r + c;
-
-        rowLit.put(r, rowLit.get(r) - 1);
-        colLit.put(c, colLit.get(c) - 1);
-        descendingDiagonalLit.put(a_d, descendingDiagonalLit.get(a_d) - 1);
-        ascendingDiagonalLit.put(d_d, ascendingDiagonalLit.get(d_d) - 1);
-    }
 }
