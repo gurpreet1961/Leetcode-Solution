9d568341b2023b563e6a48a66310a47ccc408777
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-09-06 23:41:59 +0530

    Time: 24 ms (6.54%) | Memory: 46.2 MB (56.12%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -3,23 +3,25 @@ class Solution {
         TreeMap<Integer, Integer>hm = new TreeMap<>(); //it store element in stored order of key
         int sum = 0;
         int n = wall.size();
+        int max = 0;
         for(int i = 0; i < n; i++){
             sum = 0;
             for(int j = 0;j < wall.get(i).size()-1; j++){
                 sum += wall.get(i).get(j);
                 hm.put(sum, hm.getOrDefault(sum,0)+1);
+                max = Math.max(max, hm.get(sum));
             }
         }
-        if(hm.size() == 0){
-            return n;
-        }
-        int ans = Integer.MAX_VALUE;
-        int count = 0 ;
-        for( Map.Entry<Integer,Integer> m: hm.entrySet() ){
-            ans = Math.min(ans, n - m.getValue());
-        }
-        System.out.print(hm);
-        return ans;
+        // if(hm.size() == 0){
+        //     return n;
+        // }
+        // int ans = Integer.MAX_VALUE;
+        // int count = 0 ;
+        // for( Map.Entry<Integer,Integer> m: hm.entrySet() ){
+        //     ans = Math.min(ans, n - m.getValue());
+        // }
+        // System.out.print(hm);
+        return n-max;
 
     }
 }
