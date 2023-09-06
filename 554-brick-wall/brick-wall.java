7fd1517c7dde2233a39d1957684d73c18c8235ec
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-09-06 23:36:31 +0530

    Time: 33 ms (5.35%) | Memory: 47.9 MB (5.11%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -1,23 +1,21 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-        TreeMap<Long, Integer>hm = new TreeMap<>(); //it store element in stored order of key
-        long sum = 0;
-        for(int i = 0; i < wall.size(); i++){
+        TreeMap<Integer, Integer>hm = new TreeMap<>(); //it store element in stored order of key
+        int sum = 0;
+        int n = wall.size();
+        for(int i = 0; i < n; i++){
             sum = 0;
-            for(int j = 0;j < wall.get(i).size(); j++){
+            for(int j = 0;j < wall.get(i).size()-1; j++){
                 sum += wall.get(i).get(j);
                 hm.put(sum, hm.getOrDefault(sum,0)+1);
             }
         }
+        if(hm.size() == 0){
+            return n;
+        }
         int ans = Integer.MAX_VALUE;
-        int n = wall.size();
         int count = 0 ;
-        for( Map.Entry<Long,Integer> m: hm.entrySet() ){
-            if(m.getKey() == sum){
-                ans = Math.min(ans,m.getValue());
-                break;
-            }
-            // System.out.print(val);
+        for( Map.Entry<Integer,Integer> m: hm.entrySet() ){
             ans = Math.min(ans, n - m.getValue());
         }
         System.out.print(hm);
