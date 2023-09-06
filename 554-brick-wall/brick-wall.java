b5c35060e64c93d5b06655954c21f75271ed5aba
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-09-06 23:43:58 +0530

    Time: 25 ms (6.30%) | Memory: 46.6 MB (35.67%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -1,5 +1,29 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
+
+        /*---------------------------First Approch------------------------------- */
+        // TreeMap<Integer, Integer>hm = new TreeMap<>(); //it store element in stored order of key
+        // int sum = 0;
+        // int n = wall.size();
+        // for(int i = 0; i < n; i++){
+        //     sum = 0;
+        //     for(int j = 0;j < wall.get(i).size()-1; j++){
+        //         sum += wall.get(i).get(j);
+        //         hm.put(sum, hm.getOrDefault(sum,0)+1);
+        //     }
+        // }
+        // if(hm.size() == 0){
+        //     return n;
+        // }
+        // int ans = Integer.MAX_VALUE;
+        // int count = 0 ;
+        // for( Map.Entry<Integer,Integer> m: hm.entrySet() ){
+        //     ans = Math.min(ans, n - m.getValue());
+        // }
+        // System.out.print(hm);
+        // return ans;
+
+        /*--------------------------------Second Approch------------------------------------- */
         TreeMap<Integer, Integer>hm = new TreeMap<>(); //it store element in stored order of key
         int sum = 0;
         int n = wall.size();
@@ -12,16 +36,6 @@ class Solution {
                 max = Math.max(max, hm.get(sum));
             }
         }
-        // if(hm.size() == 0){
-        //     return n;
-        // }
-        // int ans = Integer.MAX_VALUE;
-        // int count = 0 ;
-        // for( Map.Entry<Integer,Integer> m: hm.entrySet() ){
-        //     ans = Math.min(ans, n - m.getValue());
-        // }
-        // System.out.print(hm);
         return n-max;
-
     }
 }
