c2da9e02806a49d1a7a62ba720d0fa9d57f6e3a9
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-09-06 23:28:26 +0530

    Time: 40 ms (5.35%) | Memory: 48 MB (5.11%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -1,14 +1,11 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-        TreeMap<Long, Integer>hm = new TreeMap<>();
+        TreeMap<Long, Integer>hm = new TreeMap<>(); //it store element in stored order of key
         long sum = 0;
         for(int i = 0; i < wall.size(); i++){
             sum = 0;
             for(int j = 0;j < wall.get(i).size(); j++){
                 sum += wall.get(i).get(j);
-                // if(sum > Integer.MAX_VALUE){
-                //     sum = Integer.MAX_VALUE;
-                // }
                 hm.put(sum, hm.getOrDefault(sum,0)+1);
             }
         }
