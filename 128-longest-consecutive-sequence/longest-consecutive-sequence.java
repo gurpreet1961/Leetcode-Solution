08009de09c5983377964f051d49609908bfde859
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-09-08 23:59:06 +0530

    Time: 32 ms (57.65%) | Memory: 56.9 MB (57.56%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,29 +1,54 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        TreeSet<Integer> ts = new TreeSet<>();
+        /**Using Tree set to store in sorted order */
+        // TreeSet<Integer> ts = new TreeSet<>();
+        // for(int ele: nums){
+        //     ts.add(ele);
+        // }
+        // System.out.print(ts);
+        // int max = 0;
+        // int cur = 0;
+        // long count = 1;
+        // int prev = 0;
+        // for(int ele: ts){
+        //     if(count ==1){
+        //         count++;
+        //         cur++;
+        //     }else{
+        //         if(ele-1 == prev){
+        //             cur++;
+        //         }else{
+        //             max = Math.max(max,cur);
+        //             cur = 1;
+        //         }
+        //     }
+        //     prev = ele;
+        // }
+        // max = Math.max(max,cur);
+        // return max;
+
+        /**OPTIMAL SOLUTION */
+        HashSet<Integer> hs = new HashSet<>();
+
         for(int ele: nums){
-            ts.add(ele);
+            hs.add(ele);
         }
-        System.out.print(ts);
         int max = 0;
-        int cur = 0;
-        long count = 1;
-        int prev = 0;
-        for(int ele: ts){
-            if(count ==1){
-                count++;
-                cur++;
+        for(int ele: hs){
+            if(hs.contains(ele-1)){
+                continue;
             }else{
-                if(ele-1 == prev){
-                    cur++;
-                }else{
-                    max = Math.max(max,cur);
-                    cur = 1;
+                int count = 1;
+                while(true){
+                    if(hs.contains(ele+count)){
+                        count++;
+                    }else{
+                        max = Math.max(count , max);
+                        break;
+                    }
                 }
             }
-            prev = ele;
         }
-        max = Math.max(max,cur);
         return max;
     }
 }
