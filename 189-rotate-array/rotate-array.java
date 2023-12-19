34de55fad19673146587eaf951bec520b515ee43
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2023-12-19 21:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (50.16%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,48 +1,19 @@
 class Solution {
-    public void reverse(int[] nums, int s, int e){
+    private void rev(int[] nums, int s, int e){
         int n = (s+e)/2;
-
-        for(int i = s; i<=n; i++){
+        for(int i = s; i <= n ; i++){
             int temp = nums[i];
             nums[i] = nums[e];
             nums[e] = temp;
-            e--; 
-        }
-        for(int i: nums){
-            
-            System.out.print(i);
+            e--;
         }
 
     }
     public void rotate(int[] nums, int k) {
-        int n  = nums.length;
+        int n = nums.length;
         k = k%n;
-        /*-----------Method 1---------------- */
-        // while(k-->0){
-        //     int temp = nums[n-1];
-        //     for(int i = n-1; i>0; i--){
-        //         nums[i] = nums[i-1];
-        //     }
-        //     nums[0] = temp;
-        // }
-
-        /*-----------Method 2---------------- */
-        // int arr[] = new int[k];
-        // int j = 0;
-        // for(int i = n-k;i<n; i++){
-        //     arr[j++]  = nums[i];
-        // }
-        // j = n-1;
-        // for(int i = n-k-1; i>=0;i--){
-        //     nums[j--] = nums[i];
-        // }
-        // for(int i = 0 ; i < k; i++){
-        //     nums[i] = arr[i];
-        // }
-
-        /*---------------Method 3------------------- */
-        reverse(nums,0,n-k-1);
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
+        rev(nums,0,n-k-1);
+        rev(nums, n-k, n-1);
+        rev(nums,0,n-1);
     }
 }
