17e62ee92f550aaa8fbf59ca865fa28211e0ab4c
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2024-01-02 17:48:13 +0530

    Time: 2 ms (17.89%) | Memory: 45.4 MB (22.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,22 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int right[] = new int[n]; // store the max element from the right side
-        int left[] = new int[n]; //store the max element from the left side
-        right[n-1]  = height[n-1];
+        int left[] = new int[n];
+        int right[] = new int[n];
         left[0] = height[0];
+        right[n-1] = height[n-1];
 
-        //Finding the largest element from left side and from right side
-        for(int i =1,j = n-2;i<n;i++,j--){
-            int le = height[i];
+        for(int i = 1, j = n-2; i < n; i++,j--){
+            int curLeft = height[i];
+            int curRight = height[j];
 
-            int re = height[j];
-
-            left[i] = Math.max(le,left[i-1]);
-            right[j] = Math.max(re,right[j+1]);
+            left[i] = Math.max(curLeft, left[i-1]);
+            right[j] = Math.max(curRight, right[j+1]);
         }
-
-        //comparing the value of both max height at i index and take minimum one and exclude current height
         int ans = 0;
         for(int i = 0; i < n; i++){
             ans += Math.min(left[i],right[i]) - height[i];
         }
-
         return ans;
     }
 }
