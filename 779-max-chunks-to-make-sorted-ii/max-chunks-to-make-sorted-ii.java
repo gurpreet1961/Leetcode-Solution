87c51b587bc7882d70047284a3b44dcba1c593bb
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2024-01-07 18:51:18 +0530

    Time: 1 ms (42.86%) | Memory: 44.3 MB (20.15%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
@@ -1,22 +1,17 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
         int n = arr.length;
-        int[] maxOfLeft = new int[n];
-        int[] minOfRight = new int[n];
-
-        maxOfLeft[0] = arr[0];
-        minOfRight[n-1] = arr[n-1];
-        for(int i = 1;i<n;i++){
-            maxOfLeft[i] = Math.max(maxOfLeft[i-1],arr[i]);
+        int dp1[] = new int[n];
+        dp1[0] = arr[0];
+        int dp2[] = new int[n];
+        dp2[n-1] = arr[n-1];
+        for(int i = 1,j=n-2 ; i < n; i++,j--){
+            dp1[i] = Math.max(arr[i],dp1[i-1]);
+            dp2[j] = Math.min(arr[j], dp2[j+1]);
         }
-
-        for(int i = n-2; i>=0; i--){
-            minOfRight[i] = Math.min(minOfRight[i+1],arr[i]);
-        }
-
         int ans =1;
-        for(int i =0; i <n-1;i++){
-            if(maxOfLeft[i]<=minOfRight[i+1]){
+        for(int i = 0 ; i < n-1; i++){
+            if(dp1[i] <=  dp2[i+1]){
                 ans++;
             }
         }
